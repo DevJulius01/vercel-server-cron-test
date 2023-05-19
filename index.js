4a0b563d17635197a67b05b5cbe928971c9782cd
DevJulius01 <devjulius01@gmail.com>
2023-05-18 20:12:38 -0500

    bug fixed

---
+++ index.js
@@ -0,0 +1,15 @@
+const server = http.createServer((request, response) => {
+    // Capturando la URL de la solicitud
+    const url = new URL(request.url, `http://${request.headers.host}/script`);
+
+    // Capturando un parámetro específico llamado "param"
+    const paramValue = url.searchParams.get('param');
+
+    // Enviando el valor del parámetro como respuesta
+    response.end(`El valor del parámetro es: ${paramValue}`);
+    if (paramValue === process.env.TOKEN) server.close(() => { console.log('servidor detenido') })
+});
+
+server.listen(3000, () => {
+    console.log('Servidor en funcionamiento en el puerto 3000');
+});
