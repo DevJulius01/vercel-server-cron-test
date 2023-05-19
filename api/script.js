fbdeefefbe8138a1c6c0bd90ee45da8a88b3bb88
DevJulius01 <devjulius01@gmail.com>
2023-05-18 19:31:30 -0500

    with server

--- api/script.js
+++ api/script.js
@@ -1,3 +1,18 @@
+const http = require('http');
+
 export default function script() {
-    console.log("lo hicimos?")
+    const server = http.createServer((request, response) => {
+        // Capturando la URL de la solicitud
+        const url = new URL(request.url, `http://${request.headers.host}`);
+
+        // Capturando un parámetro específico llamado "param"
+        const paramValue = url.searchParams.get('param');
+
+        // Enviando el valor del parámetro como respuesta
+        response.end(`El valor del parámetro es: ${paramValue}`);
+    });
+
+    server.listen(3000, () => {
+        console.log('Servidor en funcionamiento en el puerto 3000');
+    });
 }
