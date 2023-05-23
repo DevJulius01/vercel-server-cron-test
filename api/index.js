c173a256db622d1a95037bcc313ac4f2cb6ea3b4
DevJulius01 <devjulius01@gmail.com>
2023-05-23 15:36:52 -0500

    last test before die

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
-const app = require("./api/app");
+const app = require("../utils/app");
 
 // Iniciar el servidor
 const server = app.listen(3000, () => {
     console.log('Servidor en funcionamiento en el puerto 3000');
 });
+
+module.exports = app;
