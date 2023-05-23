99a96816f1f27c97c534b2bd8eff13965a2910de
DevJulius01 <devjulius01@gmail.com>
2023-05-23 15:12:03 -0500

    mm

--- api/cron/script.js
+++ api/cron/script.js
@@ -1,15 +1,15 @@
 /* import * as dotenv from 'dotenv'
 dotenv.config() */
-const axios = require('axios')
+const axios = require('axios/dist/node/axios.cjs');
 const { ACCESS_KEY } = process.env
 
 export default function script(req, res) {
-    /*     axios.get(`http://localhost:3000/exec?access=${ACCESS_KEY}`).then((result) => {
-    
-            console.log(result);
-        }).catch((err) => {
-            console.log(err)
-        }); */
+    axios.get(`http://localhost:3000/exec?access=${ACCESS_KEY}`).then((result) => {
+
+        console.log(result);
+    }).catch((err) => {
+        console.log(err)
+    });
     console.log("hola")
 }
 
