9ee499ce648fcb2d7af75dc181679c771774cdc0
DevJulius01 <devjulius01@gmail.com>
2023-05-23 14:01:24 -0500

    testing

--- api/cron/script.js
+++ api/cron/script.js
@@ -1,19 +1,15 @@
 /* import * as dotenv from 'dotenv'
 dotenv.config() */
-const axios = require('axios')
+import { get } from 'axios';
 const { ACCESS_KEY } = process.env
 
-function script() {
-    try {
-        axios.get(`http://localhost:3000/exec?access=${ACCESS_KEY}`).then((result) => {
+function script(req, res) {
+    get(`http://localhost:3000/exec?access=${ACCESS_KEY}`).then((result) => {
 
-            console.log(result);
-        }).catch((err) => {
-            console.log(err)
-        });
-    } catch (error) {
-        console.error('Error al hacer la llamada:', error);
-    }
+        console.log(result);
+    }).catch((err) => {
+        console.log("hubo un error", err)
+    });
 }
 
 
