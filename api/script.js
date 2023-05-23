e5d58e26eb298a80cf1550cac0966da759c881cc
Jahleel-cito <jahleel.solano@julius2grow.com>
2023-05-23 11:47:03 -0500

    cron como disparador de peticion

--- api/script.js
+++ api/script.js
@@ -1,3 +1,10 @@
 
-export default function script() {
+async function script() {
+    // Lógica de la función asincrónica
+    console.log('Ejecutando función...');
+    // Simulando una tarea asincrónica
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log('Función finalizada.');
 }
+
+module.exports = script;
---
+++ cron/script.js
@@ -0,0 +1,14 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+const { ACCESS_KEY } = process.env
+
+export default async function script() {
+    try {
+        const response = await axios.get(`http://localhost:3000/exec?access=${ACCESS_KEY}`);
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error al hacer la llamada:', error);
+    }
+}
+
+
