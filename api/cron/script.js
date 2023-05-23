da9eb07c43070e3e11b1553f8b9db37281e91c07
DevJulius01 <devjulius01@gmail.com>
2023-05-23 15:00:24 -0500

    bugs and more bugs

--- api/cron/script.js
+++ api/cron/script.js
@@ -3,13 +3,14 @@ dotenv.config() */
 const axios = require('axios')
 const { ACCESS_KEY } = process.env
 
-function script(req, res) {
-    axios.get(`http://localhost:3000/exec?access=${ACCESS_KEY}`).then((result) => {
-
-        console.log(result);
-    }).catch((err) => {
-        console.log(err)
-    });
+export default function script(req, res) {
+    /*     axios.get(`http://localhost:3000/exec?access=${ACCESS_KEY}`).then((result) => {
+    
+            console.log(result);
+        }).catch((err) => {
+            console.log(err)
+        }); */
+    console.log("hola")
 }
 
 
